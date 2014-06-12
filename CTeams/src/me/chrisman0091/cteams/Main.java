65bf3b471aa246e9feb31d76bb0c6f5d9968df8b
Chris <lavacraft219@gmail.com>
2014-06-12 17:50:33 -0400

    First commit.
    Change-Id: I63539b7e716b58cc69a2bf250533c61235adb58a
    Signed-off-by: Chris <lavacraft219@gmail.com>

---
+++ CTeams/src/me/chrisman0091/cteams/Main.java
@@ -0,0 +1,13 @@
+package me.chrisman0091.cteams;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin{
+	
+	public void onEnable(){
+		
+	}
+	
+	public void onDisable(){}
+
+}
